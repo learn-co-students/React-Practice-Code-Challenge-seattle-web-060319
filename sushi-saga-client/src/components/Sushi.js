93e57229a9171b8009ba56e10cd8302f26552ac3
github username <github email address>
2019-08-11 17:26:45 -0700

    Done.

--- sushi-saga-client/src/components/Sushi.js
+++ sushi-saga-client/src/components/Sushi.js
@@ -1,23 +1,33 @@
 import React, { Fragment } from 'react'
 
-const Sushi = (props) => {
-  return (
-    <div className="sushi">
-      <div className="plate" 
-           onClick={/* Give me a callback! */ null}>
-        { 
-          /* Tell me if this sushi has been eaten! */ 
-          true ?
-            null
-          :
-            <img src={/* Give me an image source! */} width="100%" />
-        }
+class Sushi extends React.Component {
+
+  handleEatClick = () => {
+    if (this.props.remaining - this.props.sushi.price >= 0) {
+      this.props.handleEatArrayClick();
+      this.props.handleRemainingClick(this.props.sushi.price);
+      this.props.handleShowSushiClick(this.props.sushi.id);
+    }
+  }
+
+  render () {
+    return (
+      <div className="sushi">
+        <div className="plate"
+            onClick={this.handleEatClick}>
+          {
+            !this.props.sushi.img_url ?
+              null
+            :
+              <img src={this.props.sushi.img_url} width="100%" />
+          }
+        </div>
+        <h4 className="sushi-details">
+          {this.props.sushi.name} - ${this.props.sushi.price}
+        </h4>
       </div>
-      <h4 className="sushi-details">
-        {/* Give me a name! */} - ${/* Give me a price! */}
-      </h4>
-    </div>
-  )
+    )
+  }
 }
 
 export default Sushi
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,19 +1,29 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from "../components/Sushi";
 
 const SushiContainer = (props) => {
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
+        {props.sushis.map((sushi) => {
+          return (
+            <Sushi
+              sushi={sushi}
+              key={sushi.id}
+              handleShowSushiClick={props.handleShowSushiClick}
+              handleEatArrayClick={props.handleEatArrayClick}
+              handleRemainingClick={props.handleRemainingClick}
+              remaining={props.remaining}
+            />
+          );
+        })}
+        <MoreButton
+          handleMoreClick={props.handleMoreClick}
+        />
       </div>
     </Fragment>
-  )
+  );
 }
 
 export default SushiContainer
